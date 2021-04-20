2620320ead4438e313bafe9f2e8c00c32f9a3970
Miau Lightouch <miaulightouch@example.com>
2021-04-21 02:07:59 +0800

    fix: layout

--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,26 @@ let preTime = performance.now() | 0;
 let dotCount = 0;
 const handler = () => {
   requestAnimationFrame(handler);
-  const time = performance.now() | 0;
-  times.push(time - preTime);
+  const pTtime = performance.now() | 0;
+  times.push(pTtime - preTime);
   if (times.length > 100) times.shift();
   // const average = times.reduce((p, c) => p + c) / times.length;
   const average = Math.pow(times.reduce((p, c) => p * c), 1 / times.length);
   const framerate = 1000 / average;
-  fps.innerHTML = `${framerate.toFixed(3)} fps`;
+  fps.innerHTML = `Display: ${framerate.toFixed(3)} fps`;
   const int = Math.floor(framerate);
   if (dots.children.length !== int) {
     let html = '';
     for (let index = 0; index < int; index++) html += genDots(index + 1);
     dots.innerHTML = html;
   }
+
+  const time = new Date();
+  const hour = time.getHours().toString().padStart(2, '0');
+  const minute = time.getMinutes().toString().padStart(2, '0');
+  const second = time.getSeconds().toString().padStart(2, '0');
+  const ms = time.getMilliseconds().toString().padStart(3, '0');
+  timer.innerHTML = `${hour}:${minute}:${second}:${ms}`;
 
   const activeElem = document.querySelector('.active')!;
   activeElem?.classList.remove('active');
@@ -33,7 +40,7 @@ const handler = () => {
   const elem = document.querySelector(`.dot-${dotCount}`);
   elem?.classList.add('active');
 
-  preTime = time;
+  preTime = pTtime;
 }
 requestAnimationFrame(handler);
 
@@ -41,17 +48,11 @@ const internalTimes: number[] = [];
 let internalPreTime = performance.now() | 0;
 setInterval(() => {
   const pTime = performance.now() | 0;
-  const time = new Date();
-  const hour = time.getHours().toString().padStart(2, '0');
-  const minute = time.getMinutes().toString().padStart(2, '0');
-  const second = time.getSeconds().toString().padStart(2, '0');
-  const ms = time.getMilliseconds().toString().padStart(3, '0');
-  timer.innerHTML = `${hour}:${minute}:${second}:${ms}`;
 
   internalTimes.push(pTime - internalPreTime);
   if (internalTimes.length > 100) internalTimes.shift();
   // const average = internalTimes.reduce((p, c) => p + c) / internalTimes.length;
-  const average = Math.pow(internalTimes.reduce((p, c) => p * c), 1.0 / internalTimes.length);
-  internal.innerHTML = `${(1000 / average).toFixed(3)} fps`
+  const average = Math.pow(internalTimes.reduce((p, c) => p * c), 1 / internalTimes.length);
+  internal.innerHTML = `Internal: ${(1000 / average).toFixed(3)} fps`
   internalPreTime = pTime;
 });
