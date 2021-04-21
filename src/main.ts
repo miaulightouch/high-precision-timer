770123c26044df0b182a566aab008b769f7775a7
Miau Lightouch <miaulightouch@example.com>
2021-04-22 01:34:01 +0800

    refactor: performance

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ const timer = document.querySelector<HTMLDivElement>('#timer')!
 const fps = document.querySelector<HTMLSpanElement>('#fps')!
 const dots = document.querySelector<HTMLDivElement>('#dots')!
 
-const genDots = (id: number) => `<div class="dot dot-${id}">${id.toString().padStart(2, '0')}</div>`;
-
 const times: number[] = [];
 let preTime = performance.now() | 0;
 let dotCount = 0;
@@ -14,17 +12,27 @@ const handler = () => {
   const pTtime = performance.now() | 0;
   times.push(pTtime - preTime);
   preTime = pTtime;
-  if (times.length < 100) return;
+  if (times.length <= 100) return;
   times.splice(0, times.length - 100);
-  const average = times.reduce((p, c) => p + c) / times.length; // Arithmetic mean
-  // const average = Math.exp(times.reduce((p, c) => p + Math.log(c), 0) / times.length); // Geometric mean
+  // const average = times.reduce((p, c) => p + c) / times.length; // Arithmetic mean
+  const average = Math.exp(times.reduce((p, c) => p + Math.log(c), 0) / times.length); // Geometric mean
   const framerate = 1000 / average;
   fps.innerHTML = `${framerate.toFixed(3)} fps`;
+
   const totalDots = Math.round(framerate);
   if (dots.children.length !== totalDots) {
-    let html = '';
-    for (let index = 0; index < totalDots; index++) html += genDots(index + 1);
-    dots.innerHTML = html;
+    if (dots.children.length > totalDots) {
+      for (let index = totalDots; index <= dots.children.length; index++) {
+        dots.removeChild(dots.children[index]);
+      }
+    } else {
+      for (let index = dots.children.length; index <= totalDots; index++) {
+        const dot = document.createElement('div');
+        dot.classList.add('dot', `dot-${index}`);
+        dot.innerText = `${index + 1}`.padStart(2, '0');
+        dots.appendChild(dot);
+      }
+    }
   }
 
   const time = new Date();
@@ -32,7 +40,7 @@ const handler = () => {
   const minute = time.getMinutes().toString().padStart(2, '0');
   const second = time.getSeconds().toString().padStart(2, '0');
   const ms = time.getMilliseconds().toString().padStart(3, '0');
-  timer.innerHTML = `${hour}:${minute}:${second}:${ms}`;
+  timer.innerHTML = `${hour}:${minute}:${second}.${ms}`;
 
   document.querySelector('.active')?.classList.remove('active');
   dotCount += 1;
